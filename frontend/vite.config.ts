df4a3f39e879958d62a4e88bcb6edc96d3d1793b
nhatdaithienvuong <130645903+nhatdaithienvuong@users.noreply.github.com>
2024-02-09 19:57:10 +0700

    fix error build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: {},
-    "typeof process !== 'undefined'": "true",
   },
   optimizeDeps: {
     exclude: ['js-big-decimal']
